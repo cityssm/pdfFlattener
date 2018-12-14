1d84701ffad25debaf2bf14b30b309ec156789ed
dangowans <d.gowans@cityssm.on.ca>
2018-12-14 09:51:28 -0500

    Global options, code cleanup

--- src/ca/saultstemarie/pdfflattener/FlattenPDF.java
+++ src/ca/saultstemarie/pdfflattener/FlattenPDF.java
@@ -2,6 +2,7 @@ package ca.saultstemarie.pdfflattener;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Date;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -10,7 +11,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.PDPageTree;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
@@ -18,6 +18,9 @@ import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class FlattenPDF {
+	
+	public static int IMAGE_DPI = 200;
+	public static boolean SHOW_DEBUG = true;
 
 	public static void main(String[] args) {
 		
@@ -29,13 +32,16 @@ public class FlattenPDF {
 		 */
 		
 		JFileChooser fileChooser = new JFileChooser();
+		
+		fileChooser.setDialogTitle("Select the Source PDF File to Flatten");
+		fileChooser.setApproveButtonText("Set Source PDF");
 		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("PDF Documents", "pdf"));
 		fileChooser.setAcceptAllFileFilterUsed(false);
 		
 		int returnValue = fileChooser.showOpenDialog(null);
 		
 		if (returnValue != JFileChooser.APPROVE_OPTION) {
-			JOptionPane.showMessageDialog(null, "No source file selected.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "No Source PDF file selected.", "Error", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
 		}
 		
@@ -47,25 +53,35 @@ public class FlattenPDF {
 
 		File destinationFile = new File(sourceFile.getAbsolutePath() + File.separator + "FLAT-" + sourceFile.getName());
 		
+		fileChooser.setDialogTitle("Select the Destination PDF File");
+		fileChooser.setApproveButtonText("Set Destination PDF");
 		fileChooser.setSelectedFile(destinationFile);
 		
 		returnValue = fileChooser.showSaveDialog(null);
 		
 		if (returnValue != JFileChooser.APPROVE_OPTION) {
-			JOptionPane.showMessageDialog(null, "No destination file selected.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "No Destination PDF file selected.", "Error", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
 		}
 		
 		destinationFile = fileChooser.getSelectedFile();
 		
 		if (sourceFile.getPath().equals(destinationFile.getPath()) && sourceFile.getName().equals(destinationFile.getName())) {
-			JOptionPane.showMessageDialog(null, "You cannot select the same file as both the source and the destination.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "You cannot select the same PDF file as both the source and the destination.", "Error", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
 		}
 		
 		/*
 		 * Do the flattening
 		 */
+		
+		JOptionPane.showMessageDialog(null, "You are about to flatten\n" + 
+				sourceFile.getAbsolutePath() + "\n" + 
+				"into the new file\n" +
+				destinationFile.getAbsolutePath() + "\n\n" +
+				"This will run in the background.  Please be patient as it may take some time.",
+				"Ready to Start", 
+				JOptionPane.INFORMATION_MESSAGE);
 
 		returnValue = flattenPDF (sourceFile, destinationFile);
 		
@@ -77,6 +93,15 @@ public class FlattenPDF {
 		}
 	}
 
+	
+	/**
+	 * Takes a PDF file and flattens it into a new PDF file.
+	 * The new PDF file is a series of images generated from the source PDF file.
+	 * No metadata from the source PDF file is copied into the new PDF file.
+	 * @param sourceFile - The source PDF file to be flattened.
+	 * @param destinationFile - The output PDF file.
+	 * @return - 0 if successful
+	 */
 	public static int flattenPDF (File sourceFile, File destinationFile) {
 		
 		PDDocument sourceDoc = null;
@@ -87,11 +112,17 @@ public class FlattenPDF {
 			sourceDoc = PDDocument.load(sourceFile);
 			PDFRenderer pdfRenderer = new PDFRenderer(sourceDoc);
 			
-			PDPageTree pageTree = sourceDoc.getDocumentCatalog().getPages();
+			final int pageCount = sourceDoc.getDocumentCatalog().getPages().getCount();
 			
-			for (int i = 0; i < pageTree.getCount(); i += 1) {
+			log(pageCount + " page" + (pageCount == 1 ? "" : "s") + " to flatten.");
+			
+			for (int i = 0; i < pageCount; i += 1) {
 				
-				BufferedImage img = pdfRenderer.renderImageWithDPI(i, 300, ImageType.RGB);
+				log("Flattening page " + (i + 1) + " of " + pageCount + "...");
+				
+				BufferedImage img = pdfRenderer.renderImageWithDPI(i, IMAGE_DPI, ImageType.RGB);
+				
+				log("  Image rendered in memory (" + img.getWidth() + "x" + img.getHeight() + " " + IMAGE_DPI + "DPI).  Adding to PDF...");
 				
 				PDPage imagePage = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
 				destDoc.addPage(imagePage);
@@ -101,13 +132,37 @@ public class FlattenPDF {
 				PDPageContentStream imagePageContentStream = new PDPageContentStream(destDoc, imagePage);
 				imagePageContentStream.drawImage(imgObj, 0, 0);
 				
+				log("  Image added successfully.");
+				
 				imagePageContentStream.close();
+				
+				img.flush();
+				img = null;
 			}
 			
+			log("New flattened PDF created in memory.");
+			
+			
+			/*
+			 * Remove links to the source document before saving.
+			 * (Get back as much memory as possible.)
+			 */
+			
+			pdfRenderer = null;
+			
+			sourceDoc.close();
+			sourceDoc = null;
+			
+			/*
+			 * Write the new PDF file
+			 */
+			
+			log("Saving new flattened PDF...");
 			
 			destDoc.save(destinationFile);
+			destDoc.close();
 			
-		
+			log("Saved successfully.");
 		}
 		catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
@@ -131,5 +186,11 @@ public class FlattenPDF {
 		}
 		
 		return 0;
+	}
+	
+	private static void log(String message) {
+		if (SHOW_DEBUG) {
+			System.out.println(new Date().toString() + " - " + message);
+		}
 	}
 }
